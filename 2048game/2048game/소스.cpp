619a10edf6dd1878109958d12a5e9ec40e33c332
sooooooooi <suinm97@gmail.com>
2021-11-10 00:23:31 +0900

    게임판 토대 완성

---
+++ 2048game/2048game/소스.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+int score = 0;	// 점수
+int board[4][4] = { 0 };
+
+void print_board();
+void get_num();
+
+void print_board()			// 보드를 만드는 함수
+{
+	system("cls");
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (board[i][j] == 0)
+				printf(" .");
+			else
+				printf(" %d", board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("점수 : %d\n", score);
+}
+
+void get_num()				// 보드에 랜덤으로 2 또는 4를 나타나게 하는 함수
+{
+	int i, j, cnt;
+	int* p0[16] = { 0 };
+
+	for (i = 0, cnt = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+			if (board[i][j] == 0)
+			{
+				p0[cnt] = &board[i][j];
+				cnt++;
+			}
+	}
+	*p0[rand() % (cnt)] = (rand() % 100 < 80) ? 2 : 4;			// 2 또는 4를 만듦
+}
+
+int main()
+{
+	get_num();
+	get_num();
+	print_board();
+}
