38a6fa84c3c89c96deb974351dbf55518ca7fd04
choyeri <dpfl010462@gmail.com>
2021-11-11 14:33:15 +0900

    슬라이드 함수 추가

--- 2048game/2048game/소스.cpp
+++ 2048game/2048game/소스.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <windows.h>
-#include <stdlib.h>
+#include <stdlib.h>	//exit함수 사용
+#include <conio.h>
+
+//방향키 설정
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
 
 int score = 0;	// 점수
 int board[4][4] = { 0 };
@@ -9,7 +16,8 @@ int board[4][4] = { 0 };
 void print_board();
 void get_num();
 void check_Gameover();
-void slide_score();
+void slide_Board(char inputkey);
+void check_Inputkey();
 
 void print_board()			// 보드를 만드는 함수
 {
@@ -48,7 +56,7 @@ void get_num()				// 보드에 랜덤으로 2 또는 4를 나타나게 하는 함수
 	*p0[rand() % (cnt)] = ((rand() % 100) < 80) ? 2 : 4;			// 2 또는 4를 만듦
 }
 
-void check_Gameover()
+void check_Gameover()	//게임오버 체크하는 함수
 {
 	for (int i = 0; i < 4; i++) 
 	{
@@ -64,14 +72,99 @@ void check_Gameover()
 	exit(0);
 }
 
-void slide_score()
-{
+void check_Inputkey() {
+	char c;
 
+	if (_kbhit()) {
+		c = _getch();
+		if (c == -32) {
+			c = _getch();
+			slide_Board(c);
+			get_num();
+		}
+	}
 }
+
+void slide_Board(char inputkey)
+{
+	int i, j;
+
+	switch (inputkey) {
+	case LEFT:
+		for (i = 0; i < 4; i++) {
+			for (j = 3; j >= 1; j--) {
+				if (board[i][j - 1] != 0) {
+					if (board[i][j] == board[i][j - 1]) {
+						board[i][j - 1] *= 2;
+						board[i][j] = 0;
+					}
+				}
+				else {
+					board[i][j - 1] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+		break;
+	case RIGHT:
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 3; j++) {
+				if (board[i][j + 1] != 0) {
+					if (board[i][j] == board[i][j + 1]) {
+						board[i][j + 1] *= 2;
+						board[i][j] = 0;
+					}
+				}
+				else {
+					board[i][j + 1] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+		break;
+	case UP:
+		for (j = 0; j < 4; j++) {
+			for (i = 3; i >= 1; i--) {
+				if (board[i-1][j] != 0) {
+					if (board[i][j] == board[i-1][j]) {
+						board[i-1][j] *= 2;
+						board[i][j] = 0;
+					}
+				}
+				else {
+					board[i-1][j] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+		break;
+	case DOWN:
+		for (j = 0; j < 4; j++) {
+			for (i = 0; i < 3; i++) {
+				if (board[i+1][j] != 0) {
+					if (board[i][j] == board[i+1][j]) {
+						board[i+1][j] *= 2;
+						board[i][j] = 0;
+					}
+				}
+				else {
+					board[i+1][j] = board[i][j];
+					board[i][j] = 0;
+				}
+			}
+		}
+		break;
+	}
+}
+
 
 int main()
 {
 	get_num();
 	get_num();
-	print_board();
+	while(1) {
+		print_board();
+		check_Inputkey();
+		check_Gameover();
+	}
 }
